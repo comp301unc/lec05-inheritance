25f9a31352922e26d492c1fe472e3abba5a714f9
Aaron J. Smith <onsmith@cs.unc.edu>
2021-02-21 00:43:41 -0500

    initial commit

---
+++ src/main/java/com/comp301/lec05/Main.java
@@ -0,0 +1,17 @@
+package com.comp301.lec05;
+
+public class Main {
+  public static void main(String[] args) {
+    Professor emily = new Professor("Emily");
+    Student alex = new Student("Alex");
+
+    System.out.println(emily.getName());
+    System.out.println(alex.getName());
+
+    alex.takeCredits(30);
+    System.out.println(alex.getStatus()); // "Sophomore"
+
+    emily.promote();
+    System.out.println(emily.getStatus()); // "Associate"
+  }
+}
